935fe2a46399c14fa63f749d588c9795fed70f86
mrleisen <ing.rafaelcamargo@gmail.com>
2022-06-27 10:19:39 -0500

    Adding saveCharacterPicture usecase to the data layer

--- lib/data/implementations/repositoriesimpl/CharacterRepositoryImpl.dart
+++ lib/data/implementations/repositoriesimpl/CharacterRepositoryImpl.dart
@@ -7,29 +7,40 @@ import 'package:tuple/tuple.dart';
 class CharacterRepositoryImpl implements CharacterRepository{
 
   // this is the data source, if other datasources are needed then it should be added here
+  late CharacterLocalDataSource _characterLocalDataSource;
   late CharacterApiDataSource _characterApiDataSource;
 
   CharacterRepositoryImpl(
+      CharacterLocalDataSource characterLocalDataSource,
       CharacterApiDataSource characterApiDataSource
   ){
+    _characterLocalDataSource = characterLocalDataSource;
     _characterApiDataSource = characterApiDataSource;
   }
 
   @override
   Future<Tuple3<bool, List<Character>?, String>> getCharacters() async {
 
-    final resultTuple = await _characterApiDataSource.getCharacters();
-    if(resultTuple.item1){
-      return Tuple3(true, resultTuple.item2!, "");
+    // asking for the list of character to the local database
+    final localResultTuple = await _characterLocalDataSource.getCharacters();
+    if(localResultTuple.item1){
+      return Tuple3(true, localResultTuple.item2!, "");
     }else{
-      return Tuple3(false, null, resultTuple.item3);
+      // there is no characters saved locally, then get it from the api data source
+      final apiResultTuple = await _characterApiDataSource.getCharacters();
+      if(apiResultTuple.item1){
+        // save characters from api into the local database
+        await _characterLocalDataSource.saveCharacters(apiResultTuple.item2!);
+        // and return the list
+        return Tuple3(true, apiResultTuple.item2!, "");
+      }else{
+        return Tuple3(false, null, apiResultTuple.item3);
+      }
     }
   }
 
   @override
-  Future<Tuple3<bool, Character?, String>> getCharacter(String name) {
-    // TODO: implement getCharacter
-    throw UnimplementedError();
-  }
+  Future<bool> updateCharacter(Character character) async =>
+      await _characterLocalDataSource.updateCharacter(character);
 
 }
